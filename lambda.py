6a47614b90ef922405af1af493ef686956aa8a67
LP-170357\Siddharth Singh <siddharth.singh@symphonytalent.com>
2022-03-29 02:34:11 +0530

    first-commit

---
+++ lambda.py
@@ -0,0 +1,11 @@
+import requests
+
+def hellowWorld1(event, context):
+    response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
+    res = {
+        "event": event,
+        "output": response.json(),
+        "context": context
+    }
+    print(res)
+    return None
